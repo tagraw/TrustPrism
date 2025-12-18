709d490b9cbb08f96d17e07a111043874c328b91
tanvia <tanvia@Tanvis-Surface.localdomain>
2025-12-17 23:12:25 -0600

    code setup

---
+++ backend/db.js
@@ -0,0 +1,10 @@
+import pkg from "pg";
+const { Pool } = pkg;
+
+export const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: 5432,
+});
